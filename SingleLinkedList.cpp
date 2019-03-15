44ae072a25662642ceac173a78b51a6baa333f26
Caleb Tseng-Tham <cztsengt@uwaterloo.ca>
2019-03-15 16:25:57 -0400

    Updated List
    Made the list less like it was written in C by removing some un needed struct's.

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -1,31 +1,64 @@
 #include <iostream>
+#include <cstdlib>
+
+
+class Node
+{
+public:
+    Node* next;
+    int data;
+};
+
 using namespace std;
-struct Node { 
-   int data; 
-   struct Node *next; 
-}; 
-struct Node* head = NULL;   
-void insert(int new_data) { 
-   struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
-   new_node->data = new_data; 
-   new_node->next = head; 
-   head = new_node; 
-} 
-void display() { 
-   struct Node* ptr;
-   ptr = head;
-   while (ptr != NULL) { 
-      cout<< ptr->data <<" "; 
-      ptr = ptr->next; 
-   } 
-} 
-int main() { 
-   insert(3);
-   insert(1);
-   insert(7);
-   insert(2);
-   insert(9);
-   cout<<"The linked list is: ";
-   display(); 
-   return 0; 
-} 
+
+class LinkedList
+{
+public:
+    int length;
+    Node* head;
+
+    LinkedList();
+    ~LinkedList();
+    void add(int data);
+    void print();
+};
+
+LinkedList::LinkedList(){
+    this->length = 0;
+    this->head = NULL;
+}
+
+LinkedList::~LinkedList(){
+    std::cout << "LIST DELETED";
+}
+
+void LinkedList::add(int data){
+    Node* node = new Node();
+    node->data = data;
+    node->next = this->head;
+    this->head = node;
+    this->length++;
+}
+
+void LinkedList::print(){
+    Node* head = this->head;
+    int i = 1;
+    while(head){
+        std::cout << i << ": " << head->data << std::endl;
+        head = head->next;
+        i++;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    LinkedList* list = new LinkedList();
+    for (int i = 0; i < 100; ++i)
+    {
+        list->add(rand() % 100);
+    }
+    list->print();
+    std::cout << "List Length: " << list->length << std::endl;
+    delete list;
+    return 0;
+}
