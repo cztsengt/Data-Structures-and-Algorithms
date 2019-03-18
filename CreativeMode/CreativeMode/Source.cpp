0855eda1effc34de39319f1a4be8b34044cf8c81
Caleb Tseng-Tham <cztsengt@uwaterloo.ca>
2019-03-18 13:51:07 -0400

    Finished the bubble sort

--- CreativeMode/CreativeMode/Source.cpp
+++ CreativeMode/CreativeMode/Source.cpp
@@ -6,28 +6,61 @@
 using namespace std;
 
 /* 
-REFERENCE DOCUMENTS: http://www.cplusplus.com/reference/vector/vector/
+REFERENCE DOCUMENTS: 
+Vectors : http://www.cplusplus.com/reference/vector/vector/
 
+Sorting Algorithms: 
+Bubble Sort - https://www.geeksforgeeks.org/bubble-sort/
 */
+
 void vectorStuff(); 
+void bubbleSort(vector<int> & list);
+void printVector(vector<int> & list);
+void userInput();
 
 int main() {
-	vectorStuff();
+	vector<int> list = { 5, 9, 2, 4, 1, 5, 7, 0, 2, 0, 3, 9, 6, 6, 2 };
+	bubbleSort(list);
+	printVector(list);
+	userInput();
+}
+
+
+
+void bubbleSort(vector<int> & list) {
+	int size = list.size(); 
+	for (int i = 0; i < size - 1; i++) {
+	// for int (j = i; j < size - 1; j++) Problem here is that it thinks the beginning is sorted
+	// It's not. Don't forget!
+		for (int j = 0; j < size - i - 1; j++) {
+			if (list[j] > list[j + 1]) {
+				swap(list[j], list[j + 1]);
+				cout << "Swapping " << list[j] << " and " << list[j + 1] << endl;
+			}
+		}
+	}
+}
+
+void printVector(vector<int> & list) {
+	for (int i = 0; i < list.size(); i++) {
+		cout << "[" << i << "]: " << list[i] << endl; 
+	}
+}
+
+void userInput() {
+	string wait = "";
+	cin >> wait; 
 }
 
 void vectorStuff() {
 	string str_End = " ";
 	vector<int> vector_int{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	std::cout << "Lets Begin!";
-	std:cout << endl;
+	cout << "Lets Begin!";
+	cout << endl;
 	vector_int.push_back(11);
 	vector_int.push_back(12);
 	vector_int[11] = 21321313;
-	std::cout << vector_int.size() << std::endl;
-	std::cout << "Vector [11] is: " << vector_int[11] << endl;
-	std::cin >> str_End;
-}
-
-void mergeSort(vector<int> list) {
-
+	cout << vector_int.size() << endl;
+	cout << "Vector [11] is: " << vector_int[11] << endl;
+	cin >> str_End;
 }
